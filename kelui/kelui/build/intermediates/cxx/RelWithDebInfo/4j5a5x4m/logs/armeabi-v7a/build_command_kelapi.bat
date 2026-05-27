287d1c17701a0a2de6ae073555538739a0a5e1f5
mihkel365 <easyhandcoxk@atomicmail.io>
2026-05-27 07:11:28 -0400

    Delete kelui directory

--- kelui/kelui/build/intermediates/cxx/RelWithDebInfo/4j5a5x4m/logs/armeabi-v7a/build_command_kelapi.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\hcxys\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\hcxys\\Downloads\\kelui\\kelui\\.cxx\\RelWithDebInfo\\4j5a5x4m\\armeabi-v7a" ^
-  kelapi
